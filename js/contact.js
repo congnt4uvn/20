2e2e3eefe2c124d0c2952736224e3b9c659eff7f
congnt4uvn <congnt4uvn@gmail.com>
2026-09-05 22:25:38 +0700

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+// Thông tin liên hệ chung cho tất cả bất động sản
+const contactInfo = {
+  name: "MY HOUSE",
+  phone: "0358 965 708",
+  facebook: "https://facebook.com/bdsabc",
+  email: "contact@bdsabc.com",
+  address: "q7, TP.HCM",
+  zalo: "https://zalo.me/0358965708"
+};
